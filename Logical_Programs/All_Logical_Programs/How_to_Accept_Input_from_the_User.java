10218f9b75b3624480bb0c6b3185c11d67f659a7
hd888 <hd888@Himanshu>
2022-12-01 17:29:58 +0530

    [Himanshu 01Dec] : Code for Accept Input from the User

---
+++ Logical_Programs/All_Logical_Programs/How_to_Accept_Input_from_the_User.java
@@ -0,0 +1,24 @@
+package All_Logical_Programs;
+
+import java.util.Scanner;
+
+public class How_to_Accept_Input_from_the_User 
+{
+	public static void main(String[]args)
+	{
+//		System.out.println(" Enter Number : ");
+		Scanner S = new Scanner (System.in);
+//		int NUM = S.nextInt();
+//		System.out.println(NUM*NUM);
+		
+		System.out.println(" Enter Name : ");
+		String NAME = S.next();
+		System.out.println(NAME.length());
+		
+		
+		
+		
+		
+	}
+
+}
