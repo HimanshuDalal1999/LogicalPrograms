7997df869a2cc3957cee74fbe49ccc7abc0b8175
hd888 <hd888@Himanshu>
2022-12-01 17:38:35 +0530

    [Himanshu] : Code for Multiplication of Two Numbers Without Using Multiplication Operator

---
+++ Logical_Programs/All_Logical_Programs/Multiplication_without_Multiplication_Operator.java
@@ -0,0 +1,21 @@
+package All_Logical_Programs;
+
+public class Multiplication_without_Multiplication_Operator
+{
+	public static void main(String[]args)
+	{
+		int NUM1 = 12;
+		int NUM2 = 4;
+		
+		int SUM = 0;
+		
+		for(int i=1; i<=NUM2; i++)
+		{
+			SUM = SUM+NUM1;
+		}
+		
+		System.out.println(SUM);
+		
+	}
+
+}
