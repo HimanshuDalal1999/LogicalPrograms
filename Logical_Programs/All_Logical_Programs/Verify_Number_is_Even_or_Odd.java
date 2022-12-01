d08c4fad4365b4621519868847ef27f33db9b6a3
hd888 <hd888@Himanshu>
2022-12-01 17:44:34 +0530

    [Himashu] : Code for Verify the Given Number is ODD or EVEN

---
+++ Logical_Programs/All_Logical_Programs/Verify_Number_is_Even_or_Odd.java
@@ -0,0 +1,19 @@
+package All_Logical_Programs;
+
+public class Verify_Number_is_Even_or_Odd 
+{
+	public static void main(String[]args)
+	{
+		int NUM  = 18;
+		
+		if(NUM%2==0)
+		{
+			System.out.println(" Given Number "+NUM+" is EVEN");
+		}
+		else
+		{
+			System.out.println(" Given Number "+NUM+" is ODD");
+		}
+	}
+
+}
